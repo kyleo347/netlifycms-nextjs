7aa04c53004063f7727c5151936c2f3163f35aa7
Kyle <kyleoconnor347@gmail.com>
2020-08-11 09:51:50 -0700

    add material ui

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,38 +1,161 @@
 import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
+import ListIcon from '@material-ui/icons/List';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Link from 'next/link';
+import { node } from 'prop-types';
 
-const Layout = ({ children }) => {
-  return (
-    <React.Fragment>
-      <nav>
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function Layout({ children }) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
         <Link href={'/'}>
-          <a>home</a>
+          <ListItem button key="Home">
+            <ListItemIcon>
+              <HomeIcon></HomeIcon>
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
         </Link>
         <Link href={'/blog'}>
-          <a>blog</a>
+          <ListItem button key="Blog">
+            <ListItemIcon>
+              <ListIcon></ListIcon>
+            </ListItemIcon>
+            <ListItemText primary="Blog" />
+          </ListItem>
         </Link>
         <Link href={'/about'}>
-          <a>about</a>
+          <ListItem button key="About">
+            <ListItemIcon>
+              <InfoIcon></InfoIcon>
+            </ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItem>
         </Link>
-      </nav>
-      <main>{children}</main>
-      <style jsx global>{`
-        nav {
-          text-align: center;
-        }
-        nav a {
-          margin-right: 2px;
-
-          padding: 4px;
-        }
-
-        main {
-          display: flex;
-          flex-direction: column;
-        }
-      `}</style>
-    </React.Fragment>
+        {/* {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))} */}
+      </List>
+    </div>
   );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Responsive drawer
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer}>
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {children}
+      </main>
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  children: node,
 };
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import { Container } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../theme';
+import { elementType, object } from 'prop-types';
 import Layout from '../components/layout';
 
-export default class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-    return (
-      <Container>
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <Container>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </Container>
-    );
-  }
+      </ThemeProvider>
+    </Container>
+  );
 }
+
+MyApp.propTypes = {
+  Component: elementType.isRequired,
+  pageProps: object.isRequired,
+};
